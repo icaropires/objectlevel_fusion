0fe2df336e6cd1e0b3e978f8afba51e0e3129aa8
Ícaro Pires <icaropsa@gmail.com>
2021-03-30 12:00:29 -0300

    Call spatial alignment in fusion module and add basic visualization

--- fusion_layer/include/fusion_layer/fusion.hpp
+++ fusion_layer/include/fusion_layer/fusion.hpp
@@ -2,9 +2,13 @@
 
 #include <functional>
 #include <memory>
+#include <cstdio>
 
 #include "rclcpp/rclcpp.hpp"
 #include "object_model_msgs/msg/object_model.hpp"
+
+#include "fusion_layer/spatial_alignment.hpp"
+#include "types.hpp"
 
 using std::placeholders::_1;
 
@@ -15,5 +19,7 @@ public:
 
 private:
   void topic_callback(const object_model_msgs::msg::ObjectModel::SharedPtr msg) const;
+  static std::string state_to_str(const state_t& state);
+
   rclcpp::Subscription<object_model_msgs::msg::ObjectModel>::SharedPtr subscription_;
 };
--- fusion_layer/src/fusion.cpp
+++ fusion_layer/src/fusion.cpp
@@ -1,38 +1,51 @@
 #include "fusion_layer/fusion.hpp"
-#include <Eigen/Dense> // remove
-#include <string> // remove
 
 Fusion::Fusion()
   : Node("fusion_layer")
 {
-    // Make it attribute
+    // TODO: Make it attribute
     const std::string topic = "objectlevel_fusion/fusion_layer/fusion/submit";
 
     subscription_ = this->create_subscription<object_model_msgs::msg::ObjectModel>(
         topic, 10, std::bind(&Fusion::topic_callback, this, _1)
     );
+
+}
+
+std::string Fusion::state_to_str(const state_t& state) {
+    using namespace object_model_msgs::msg;
+
+    static constexpr size_t maximum_str_size = 200;
+    char c_str[maximum_str_size]; 
+
+    sprintf(c_str,
+            "X = %0.3f, Y = %0.3f, Vx = %0.3f, Vy = %0.3f, Ax = %0.3f, Ay = %0.3f, Yaw = %0.3f, Yaw Rate = %0.3f",
+            state[Track::STATE_X_IDX],
+            state[Track::STATE_Y_IDX],
+            state[Track::STATE_VELOCITY_X_IDX],
+            state[Track::STATE_VELOCITY_Y_IDX],
+            state[Track::STATE_ACCELERATION_X_IDX],
+            state[Track::STATE_ACCELERATION_Y_IDX],
+            state[Track::STATE_YAW_IDX],
+            state[Track::STATE_YAW_RATE_IDX]);
+
+    return std::string(c_str);
 }
 
 void Fusion::topic_callback(const object_model_msgs::msg::ObjectModel::SharedPtr msg) const
 {
-    RCLCPP_INFO(this->get_logger(), "Received message from: \n%s", msg->header.frame_id.c_str());
- 
-    // TODO: delete this block, letting for now just to let this run through CI to validate lib integration
-    {
-        using namespace Eigen;
-     
-        Matrix3f m1;
-        m1 << 1, 2, 3,
-              4, 5, 6,
-              7, 8, 9;
-        auto m2 = Matrix3f::Constant(1.5);
+    using namespace object_model_msgs::msg;
 
-        Matrix3f m3 = m1 * m2;
+    state_t state = msg->track.state;
 
-        std::stringstream ss;
-        ss << m3;
-        RCLCPP_INFO(this->get_logger(), "M = %s", ss.str().c_str());
-    }
+    auto state_str = state_to_str(state);
+    RCLCPP_INFO(this->get_logger(), "Received message from: %s\nState:\n %s", msg->header.frame_id.c_str(), state_str.c_str());
+
+    float x = 1.0, y = 1.0, theta = 90;
+    state_t state_spatially_aligned;
+    spatially_align(x, y, theta, state, state_spatially_aligned);
+    state_str = state_to_str(state_spatially_aligned);
+    RCLCPP_INFO(this->get_logger(), "\nState spatially aligned:\n %s", state_str.c_str());
 }
 
 // One main for each node
