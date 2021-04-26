6bddd51b85797a671a595af89f2a172699599027
Ícaro Pires <icaropsa@gmail.com>
2021-04-26 00:26:09 -0300

    Complete first usable version of simple association

--- fusion_layer/src/simple_association.cpp
+++ fusion_layer/src/simple_association.cpp
@@ -1,26 +1,80 @@
 #include "fusion_layer/simple_association.hpp"
 
-uint32_t simple_associate(const object_model_msgs::msg::Object& object,
-        const std::map<uint32_t, object_model_msgs::msg::Object::SharedPtr>& global_object_model) {
-
-    using object_model_msgs::msg::Track;
-
-    constexpr int rectangle_n_points = 4;
-
-    // TODO
-    (void) object; 
-    (void) global_object_model;
-
-    const std::array<Point, rectangle_n_points> points1{Point(1.3, 2.5), Point(2.7, 2.5), Point(2.7, 5.5), Point(1.3, 5.5)};
-    const Polygon polygon1(points1.cbegin(), points1.cend());
-
-    const std::array<Point, rectangle_n_points> points2({Point(1.47, 2.65), Point(2.63, 2.34), Point(3.3, 4.85), Point(2.14, 5.16)});
-    const Polygon polygon2(points2.cbegin(), points2.cend());
-
-    std::vector<PolygonWithHoles> intersections;
-    CGAL::intersection(polygon1, polygon2, std::back_inserter(intersections));
-
-    std::cout << "Intersection area: " << intersections[0].outer_boundary().area() << std::endl;
-
-    return 1;
+namespace SimpleAssociation {
+    uint32_t associate(const object_model_msgs::msg::Object& received,
+            const std::map<uint32_t, object_model_msgs::msg::Object::SharedPtr>& global_object_model,
+            double break_threshold,
+            double minimum_score) {
+    
+        const Polygon received_rectangle = obj_to_rectangle(received);
+    
+        uint32_t idx_biggest = global_object_model.size();
+        double biggest_score = minimum_score;
+    
+        for(const auto& global: global_object_model) {
+            const Polygon global_rectangle = obj_to_rectangle(*global.second);
+    
+            double association_score = get_association_score(received_rectangle, global_rectangle);
+            if(association_score > biggest_score){
+                idx_biggest = global.first;
+                biggest_score = association_score;
+    
+                if(biggest_score >= break_threshold) {
+                    break;
+                }
+            }
+        }
+    
+        return idx_biggest;
+    }
+    
+    double get_association_score(const Polygon& pol1, const Polygon& pol2) {
+        std::vector<PolygonWithHoles> intersections;
+        CGAL::intersection(pol1, pol2, std::back_inserter(intersections));
+    
+        const double pol1_area = CGAL::to_double(pol1.area());
+        const double pol2_area = CGAL::to_double(pol2.area());
+    
+        if(intersections.size()) {
+            const double intersection_area = CGAL::to_double(intersections[0].outer_boundary().area());
+    
+            const double intersection_coverage = intersection_area / std::min(pol1_area, pol2_area);
+            const double objs_coverage = std::min(pol1_area, pol2_area) / std::max(pol1_area, pol2_area);
+    
+            return objs_coverage * intersection_coverage;
+        }
+    
+        return 0;
+    }
+    
+    Polygon obj_to_rectangle(const object_model_msgs::msg::Object& object) {
+        using object_model_msgs::msg::Track;
+        using object_model_msgs::msg::Dimensions;
+    
+        constexpr int rectangle_n_points = 4;
+    
+        const auto& state = object.track.state;
+        const auto& dimensions = object.dimensions.values;
+    
+        const float x = state[Track::STATE_X_IDX];
+        const float y = state[Track::STATE_Y_IDX];
+        const float half_width = dimensions[Dimensions::DIMENSIONS_WIDTH_IDX]/2;
+        const float half_length = dimensions[Dimensions::DIMENSIONS_LENGHT_IDX]/2;
+    
+        const float yaw = state[Track::STATE_YAW_IDX];
+        Transformation rotate(CGAL::ROTATION, sin(yaw), cos(yaw));
+    
+        std::array<Point, rectangle_n_points> points ({
+            Point(x-half_width, y-half_length),
+            Point(x+half_width, y-half_length),
+            Point(x+half_width, y+half_length),
+            Point(x-half_width, y+half_length)
+        });
+    
+        for(auto& point: points) {
+            point = rotate(point);
+        }
+    
+        return Polygon(points.cbegin(), points.cend());
+    }
 }
